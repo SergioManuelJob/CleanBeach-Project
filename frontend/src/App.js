fa12ac6de11fb424ee0de2d78753817b4569b46b
Lydia <lydiadoula@gmail.com>
2023-03-08 15:51:32 +0000

    Image added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Image from './images/pexels-asad-photo-maldives-1450353.jpg';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
+      <img className='beachimg' src={Image} alt="Beach" />
     </React.Fragment>
   );
+
 }
 
 export default App;
