a4446e377a08080022560851a4ff6f9a9ce8ebe3
Lydia <lydiadoula@gmail.com>
2023-03-08 15:26:56 +0000

    No changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,6 +5,7 @@ function Navbar() {
         <header>
             <h3 href="#">Logo</h3>
             <nav>
+                <a href="/#">Home</a>
                 <a href="/#">Events</a>
                 <a href="/#">Sign Up</a>
                 <a href="/#">Profile</a>
