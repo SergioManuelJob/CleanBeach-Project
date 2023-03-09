f6d0ad98e6f1817443dd758f12b6bf43b18df09e
Lydia <lydiadoula@gmail.com>
2023-03-09 10:22:29 +0000

    dropdown button added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Image from './images/pexels-asad-photo-maldives-1450353.jpg';
+import Dropdown from './components/Dropdown/Dropdown';
 
 function App() {
+
+const [selected, setSelected] = useState('');
+
   return (
     <React.Fragment>
       <Navbar/>
       <img className='beachimg' src={Image} alt="Beach" />
+      <Dropdown selected={selected} setSelected={setSelected} />
     </React.Fragment>
   );
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <header>
             <h3 href="#">Logo</h3>
             <nav>
-                <a href="/#">Homme</a>
+                <a href="/#">Home</a>
                 <a href="/#">Events</a>
                 <a href="/#">Sign Up</a>
                 <a href="/#">Profile</a>
