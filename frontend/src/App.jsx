7378374a5f25d18dafec0a0f064527f4de2bc858
Emma Nielsen <Emmanielsen843@gmail.com>
2023-03-10 14:09:56 +0100

    userpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
-import Test from './components/Pages/LandingPage/LandingPage'
+import Image from './images/pexels-asad-photo-maldives-1450353.jpg';
+import Dropdown from './components/Dropdown/Dropdown';
+import UserPage from './components/Pages/UserPage/UserPage';
+
 
 function App() {
+
+const [selected, setSelected] = useState('');
+
   return (
-    <>
-      jhhghjg
-    </>
+    <React.Fragment>
+      <Navbar/>
+      <img className='beachimg' src={Image} alt="Beach" />
+      <Dropdown selected={selected} setSelected={setSelected} />
+      <UserPage />
+    </React.Fragment>
   );
+
 }
 
 export default App;
-
-
---
+++ frontend/src/components/Pages/UserPage/UserPage.jsx
@@ -0,0 +1,36 @@
+import './UserPage.scss'
+import pfp from '../../../images/pexels-asad-photo-maldives-1450353.jpg'
+import { BsPencilSquare } from 'react-icons/bs'
+
+const UserPage = () => {
+    return(
+        <>
+        <div className="profile">
+            <div>
+            <img src={pfp} alt="pfpPicture" />
+            <a href='https'><BsPencilSquare className='iconBs' /></a>
+            </div>
+        <h2>Name goes here</h2>
+        <p>Joined: </p>
+        <button>ADMIN</button>
+        </div>
+
+        <form>
+        <div className="formular">
+            <label htmlFor="events">Latest events joined</label>
+            <input type="text" name="events" id="events" />
+        </div>
+        <div>
+            <label htmlFor="rated">Beaches rated</label>
+            <input type="text" name="rated" id="rated" />
+        </div>
+        <div>
+            <label htmlFor="comment">Latest review</label>
+            <textarea name="comment" id="comment" cols="48" rows="7"></textarea>
+        </div>
+        </form>
+        </>
+    )
+}
+
+export default UserPage
