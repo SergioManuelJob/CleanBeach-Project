2bfdf98d7263403a2ce0a6d69f62abc5fb93aca0
Emma Nielsen <Emmanielsen843@gmail.com>
2023-03-14 12:59:55 +0100

    map

---
+++ frontend/src/components/Map/Map.jsx
@@ -0,0 +1,9 @@
+const Map = () => {
+    return(
+        <div className="map">
+            <iframe title="map" width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Las%20Canteras%20Las%20Palmas%20de%20Gran%20Canaria+(Las%20Canteras%20Beach)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+        </div>
+    )
+}
+
+export default Map
